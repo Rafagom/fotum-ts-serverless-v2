2d8513aa75a5ad01d0c95722d312a59f68d26cb8
Rafael <eng.rsgomes@gmail.com>
2023-01-20 18:48:03 -0300

    ae

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,18 @@ export default class Document extends NextDocument {
             <Html>
                 <Head>
                     <Script
+                        id="google-analytics"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5TJPJLF');`,
+                        }}
+                    ></Script>
+
+                    {/* <Script
                         src="https://www.googletagmanager.com/gtag/js?id=GTM-5TJPJLF"
                         strategy="afterInteractive"
                     />
@@ -18,18 +30,7 @@ export default class Document extends NextDocument {
 
           gtag('config', 'GTM-5TJPJLF');
         `}
-                    </Script>
-                    {/* <Script
-                        id="google-analytics"
-                        strategy="afterInteractive"
-                        dangerouslySetInnerHTML={{
-                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5TJPJLF');`,
-                        }}
-                    ></Script> */}
+                    </Script> */}
                     <meta
                         name="google-site-verification"
                         content="a7JLH2XUmPvgX4X7Eojev9zKBVLX0KzsZfWHQ8IMkvE"
