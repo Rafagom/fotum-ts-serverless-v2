fbd37acbf11a24127d7500a619db11745c42b55c
rafael <rafaelvasconcelos13@gmail.com>
2023-08-10 19:28:06 -0300

    inicio image changed done

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,41 +1,40 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export function Hero2() {
-	return (
-		<div className="flex flex-col  max-w-[1400px] mx-auto  h-full px-6 w-full">
-			<div className="md:grid md:grid-cols-2 md:gap-x-10 lg:gap-x-16 xl:gap-x-24 items-center justify-center h-full">
-				<div className="hidden  justify-center items-center md:grid md:col-start-1 rounded-r-3xl relative w-full h-full">
-					<Image
-						className=" rounded-3xl "
-						src={"/Sandro.jpeg"}
-						alt={"Geração residencial"}
-						layout="fill"
-					/>
-				</div>
+  return (
+    <div className="flex flex-col  max-w-[1400px] mx-auto  h-full px-6 w-full">
+      <div className="md:grid md:grid-cols-2 md:gap-x-10 lg:gap-x-16 xl:gap-x-24 items-center justify-center h-full">
+        <div className="hidden  justify-center items-center md:grid md:col-start-1 rounded-r-3xl relative w-full h-full">
+          <Image
+            className=" rounded-3xl "
+            src={'/panel-on-rooftop.jpg'}
+            alt={'Geração residencial'}
+            layout="fill"
+          />
+        </div>
 
-				<div
-					className=" flex flex-col  md:gap-6 md:h-full justify-center items-center py-2 lg:py-0"
-					data-aos="zoom-y-out"
-				>
-					<p className="px-4 text-lg md:text-base lg:text-2xl xl:text-3xl md:px-0">
-						Equipe especializada e altamente capacitada, oferecemos
-						segurança e garantia em todos os serviços prestados,
-						atendendo à todas as normas técnicas e de segurança
-						vigente. Os nossos valores estão pautados na confiança,
-						transparência e respeito.
-					</p>
+        <div
+          className=" flex flex-col  md:gap-6 md:h-full justify-center items-center py-2 lg:py-0"
+          data-aos="zoom-y-out"
+        >
+          <p className="px-4 text-lg md:text-base lg:text-2xl xl:text-3xl md:px-0">
+            Equipe especializada e altamente capacitada, oferecemos segurança e
+            garantia em todos os serviços prestados, atendendo à todas as normas
+            técnicas e de segurança vigente. Os nossos valores estão pautados na
+            confiança, transparência e respeito.
+          </p>
 
-					<div className=" grid-cols-1   max-w-[1400px] lg:text-2xl hidden md:grid">
-						<Link href="#servicos">
-							<a className="items-start py-2 px-6 bg-[white] text-[#00324b] font-bold rounded border-2 border-transparent whitespace-nowrap hidden sm:flex">
-								Saiba mais →
-							</a>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className=" grid-cols-1   max-w-[1400px] lg:text-2xl hidden md:grid">
+            <Link href="#servicos">
+              <a className="items-start py-2 px-6 bg-[white] text-[#00324b] font-bold rounded border-2 border-transparent whitespace-nowrap hidden sm:flex">
+                Saiba mais →
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,89 +1,89 @@
-import Features from "../components/ServicesDesktop";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import AboutEngineer from "../components/Ads/AboutEngineer";
-import Advantages from "../components/Ads/SolarAdvantages";
-import WorkNumbers from "../components/Ads/WorkNumbers";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
-import { Hero } from "../components/Hero";
-import { Services } from "../components/Services";
-import { KnownBrands } from "../components/KnownBrands";
-import { Hero2 } from "../components/Hero2";
-import { MobileWelcome } from "../components/MobileWelcome";
+import Features from '../components/ServicesDesktop';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import AboutEngineer from '../components/Ads/AboutEngineer';
+import Advantages from '../components/Ads/SolarAdvantages';
+import WorkNumbers from '../components/Ads/WorkNumbers';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Hero } from '../components/Hero';
+import { Services } from '../components/Services';
+import { KnownBrands } from '../components/KnownBrands';
+import { Hero2 } from '../components/Hero2';
+import { MobileWelcome } from '../components/MobileWelcome';
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<meta charSet="UTF-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0"
-				></meta>
-				<meta
-					name="description"
-					content="Com mais de 10 anos de experiência em serviços elétricos de excelência, oferecendo as melhores soluções em projetos, instalações e energia solar fotovoltaica."
-				/>
-				<title>Fótum Engenharia</title>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#ffffff"></meta>
-			</Head>
-			<div className="flex flex-col w-full overflow-hidden">
-				<Header />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="description"
+          content="Com mais de 10 anos de experiência em serviços elétricos de excelência, oferecendo as melhores soluções em projetos, instalações e energia solar fotovoltaica."
+        />
+        <title>Fótum Engenharia</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <div className="flex flex-col w-full overflow-hidden">
+        <Header />
 
-				<div className="sm:hidden flex">
-					<MobileWelcome />
-				</div>
+        <div className="sm:hidden flex">
+          <MobileWelcome />
+        </div>
 
-				<div id="hero" className="flex flex-col pt-24">
-					<div className="flex flex-col md:grid md:grid-rows-2  gap-y-10 pb-16">
-						<div className="relative ">
-							<Hero />
-						</div>
-						<div className="relative">
-							<Hero2 />
-						</div>
-					</div>
+        <div id="hero" className="flex flex-col pt-24">
+          <div className="flex flex-col md:grid md:grid-rows-2  gap-y-10 pb-16">
+            <div className="relative ">
+              <Hero />
+            </div>
+            <div className="relative">
+              <Hero2 />
+            </div>
+          </div>
 
-					<div className="hidden md:block">
-						<Features />
-					</div>
+          <div className="hidden md:block">
+            <Features />
+          </div>
 
-					<div className="md:hidden">
-						<Services />
-					</div>
+          <div className="md:hidden">
+            <Services />
+          </div>
 
-					<KnownBrands />
+          <KnownBrands />
 
-					<div className="pt-48 pb-10">
-						<AboutEngineer />
-					</div>
+          <div className="pt-48 pb-10">
+            <AboutEngineer />
+          </div>
 
-					<Footer />
-				</div>
-			</div>
-		</>
-	);
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
