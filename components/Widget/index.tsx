e596fcd7ad9790935f4de91ce9c5a588a0718271
Rafael <eng.rsgomes@gmail.com>
2023-08-14 14:59:19 -0300

    widget

--- components/Widget/index.tsx
+++ components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import { WhatsappLogo } from "phosphor-react";
+import { Phone, WhatsappLogo } from "phosphor-react";
 import { Popover } from "@headlessui/react";
 import { WidgetForm } from "./WidgetForm";
 import { useRouter } from "next/router";
@@ -7,39 +7,74 @@ export function Widget() {
 	const router = useRouter();
 
 	return (
-		<div className="fixed bottom-20 right-5 z-10 flex flex-col gap-5 justify-end items-end  ">
-			{/* <a
-				className="bg-[#00324b] rounded-full p-3 text-white items-center flex group w-fit  relative shake"
+		<div className="fixed bottom-20 right-5 z-20 flex flex-col gap-5 justify-end items-end  ">
+			<a
+				className="bg-[#00324b] rounded-full p-3 text-white items-center flex group w-fit  relative shake shadow-md"
 				href="https://wa.me/message/2KTISK2P7CG6E1"
 				rel="noreferrer"
 				target="_blank"
 			>
-				<WhatsappLogo className="w-6 h-6 lg:w-10 lg:h-10" />
+				<WhatsappLogo className="w-6 h-6 lg:w-8 lg:h-8" />
 				<div className="hidden show sm:flex">
 					<span className="max-w-0 overflow-hidden group-hover:max-w-xs duration-700">
 						&nbsp;Whatsapp
 					</span>
 				</div>
-			</a> */}
+			</a>
 
-			<a
-				className="bg-[#ffaa00] rounded-full p-3 text-white items-center flex  relative group "
-				href="https://wa.me/message/2KTISK2P7CG6E1"
-				rel="noreferrer"
-				target="_blank"
-			>
-				<div className="absolute inset-2 lg:inset-4 group-hover:inset-x-16 group-hover:inset-y-4 animate-ping  bg-[#ffaa00] lg:opacity-100 lg:group-hover:opacity-70 rounded-full -z-10"></div>
-				<div className="aspect-square h-10 lg:h-14 flex items-center justify-center flex-col">
-					<WhatsappLogo className="w-full h-full" />
-					<p className="text-sm lg:text-base">24hrs</p>
-				</div>
+			<Popover>
+				{({ open }) => (
+					<>
+						<Popover.Button className="bg-[#ffaa00] rounded-full py-3 px-5 text-white items-center flex  relative shadow-md">
+							<div
+								className={`absolute top-1/5 left-1/5 w-3/4 h-3/4  bg-[#ffaa00] lg:opacity-100  rounded-full -z-10 ${
+									open ? "animate-none" : "animate-ping"
+								}`}
+							></div>
+							<div className="h-6 flex items-center justify-center flex-col">
+								<div className="flex h-full">
+									<WhatsappLogo className="w-full h-full" />
+									<Phone className="w-full h-full" />
+								</div>
 
-				<div className="hidden show sm:flex ">
+								<p className="text-[0.6rem] lg:text-xs ">
+									Emergência 24hrs
+								</p>
+							</div>
+
+							{/* <div className="hidden show sm:flex ">
 					<span className="max-w-0 overflow-hidden group-hover:max-w-xs duration-700 whitespace-nowrap ">
 						Contato
 					</span>
-				</div>
-			</a>
+				</div> */}
+						</Popover.Button>
+						<Popover.Panel className="absolute z-40 -top-16 right-0 text-xs">
+							<div className=" grid grid-rows-2 gapy-4 h-32 bg-[#ffaa00] rounded-2xl relative">
+								<a
+									href="https://wa.me/message/2KTISK2P7CG6E1"
+									rel="noreferrer"
+									target="_blank"
+									className="flex items-center justify-start gap-2 rounded-full py-4 px-2 text-white w-40 "
+								>
+									<WhatsappLogo className="h-full w-fit" />
+									<p>WhatsApp</p>
+								</a>
+
+								<div className="h-[1px] w-full absolute bg-white top-1/2"></div>
+								<a
+									href="tel:48984110903"
+									className="flex items-center justify-start gap-2 rounded-full py-4 px-2 text-white w-40"
+								>
+									<Phone className="h-full w-fit" />
+									<p className="whitespace-nowrap">
+										(48) 98411-0903
+									</p>
+								</a>
+							</div>
+						</Popover.Panel>
+					</>
+				)}
+			</Popover>
 		</div>
 	);
 }
